867210bb00a2d7bd8e4341729f6792362b6e3fee
David Wallace <dmwallace42@lci-90.local>
2018-09-20 12:37:55 -0500

    Initial Commit

---
+++ Dyswis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dyswis
+//
+//  Created by David M Wallace on 9/20/18.
+//  Copyright © 2018 David M Wallace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
