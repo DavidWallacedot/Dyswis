70ad04ab2bb5579d019b05d7ae494f339f789b50
David Wallace <dmwallace42@lci-90.local>
2018-09-23 18:04:44 -0500

    working with view controller

---
+++ Dyswis/myCell.swift
@@ -0,0 +1,13 @@
+//
+//  myCell.swift
+//  Dyswis
+//
+//  Created by David M Wallace on 9/23/18.
+//  Copyright © 2018 David M Wallace. All rights reserved.
+//
+
+import UIKit
+
+class myCell: NSObject {
+
+}
